49821082ff1b89aed6ee73cc53c1989c09363f8a
it1925 <pavelsixta2@seznam.cz>
2020-09-17 08:29:37 +0200

    myfirstcommmit

---
+++ main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv) {
+    int i;
+    for(i=0;i<100;i++){
+       printf("umri\n"); 
+    }
+    
+
+    return (EXIT_SUCCESS);
+}
+
